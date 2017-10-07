4cc70ae5faf168c82c06cf0c7aaa1a13352836af
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2017-10-07 19:58:30 +0100

    updates to use scss files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,44 +1,9 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
-
+        <title>Mosque Tech</title>
+        <link rel="stylesheet" href="{{ asset('css/homepage.css') }}" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-
-            .title strong, p strong {
-                /* color: #009688; */
-                color: #E91E63;
-            }
-        </style>
     </head>
     <body>
         <div class="container">
