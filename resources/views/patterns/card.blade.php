3d64cd022f3e044b7ed38aebcc07a2252a2a8fd8
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2017-10-31 15:08:44 +0000

    Adds if statements

--- resources/views/patterns/card.blade.php
+++ resources/views/patterns/card.blade.php
@@ -1,14 +1,28 @@
 <div class="card" style="width: 20rem;">
-    <img src="{{ $logo }}" class="card-img-top" alt="Logo">
+    @if( isset( $logo ) )
+        <img src="{{ $logo }}" class="card-img-top" alt="Logo">
+    @endif
     <div class="card-body">
-        <h4 class="card-title">{{ $title }}</h4>
-        <h6 class="card-subtitle mb-2 text-muted">{{ $subtitle }}</h6>
-        <p class="card-text">{{ $description }}</p>
+        @if( isset( $title ) )
+            <h4 class="card-title">{{ $title }}</h4>
+        @endif
+        @if( isset( $subtitle ) )
+            <h6 class="card-subtitle mb-2 text-muted">{{ $subtitle }}</h6>
+        @endif
+        @if( isset( $description ) )
+            <p class="card-text">{{ $description }}</p>
+        @endif
     </div>
     <div class="card-footer">
-        <a href="{{ $link['href'] or '#' }}" class="btn btn-info" target="_blank">{{ $link['label'] }}</a>
+        @if( isset( $link ) )
+            <a href="{{ $link['href'] or '#' }}" class="btn btn-info" target="_blank">{{ $link['label'] }}</a>
+        @endif
         <hr>
-        <a href="{{ $additional_link['href'] or '#' }}" class="card-link" target="_blank">{{ $additional_link['label'] }}</a>
-        <a href="{{ $help_link['href'] or '#' }}" class="card-link" target="_blank">{{ $help_link['label'] }}</a>
+        @if( isset( $additional_link ) )
+            <a href="{{ $additional_link['href'] or '#' }}" class="card-link" target="_blank">{{ $additional_link['label'] }}</a>
+        @endif
+        @if( isset( $help_link ) )
+            <a href="{{ $help_link['href'] or '#' }}" class="card-link" target="_blank">{{ $help_link['label'] }}</a>
+        @endif
     </div>
 </div>
